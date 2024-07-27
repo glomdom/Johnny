c15a6de2eef1353868c90bf5ba0c5923569c1b56
glomdom <22115@uktc-bg.com>
2024-07-28 01:22:28 +0300

    fix: file scoped namespaces

--- Johnny/CodegenHelpers.cs
+++ Johnny/CodegenHelpers.cs
@@ -2,27 +2,27 @@ using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace Johnny {
-    public static class CodegenHelpers {
-        public static string GetNamespace(SyntaxNode syntaxNode) {
-            var current = syntaxNode.Parent;
+namespace Johnny;
 
-            while (current is not (NamespaceDeclarationSyntax or FileScopedNamespaceDeclarationSyntax)) {
-                current = current?.Parent;
-            }
+public static class CodegenHelpers {
+    public static string GetNamespace(SyntaxNode syntaxNode) {
+        var current = syntaxNode.Parent;
 
-            if (current is not BaseNamespaceDeclarationSyntax namespaceDeclaration) {
-                return string.Empty;
-            }
-
-            var namespaceParts = new Stack<string>();
-
-            while (namespaceDeclaration != null) {
-                namespaceParts.Push(namespaceDeclaration.Name.ToString());
-                namespaceDeclaration = namespaceDeclaration.Parent as BaseNamespaceDeclarationSyntax;
-            }
-
-            return string.Join(".", namespaceParts);
+        while (current is not (NamespaceDeclarationSyntax or FileScopedNamespaceDeclarationSyntax)) {
+            current = current?.Parent;
         }
+
+        if (current is not BaseNamespaceDeclarationSyntax namespaceDeclaration) {
+            return string.Empty;
+        }
+
+        var namespaceParts = new Stack<string>();
+
+        while (namespaceDeclaration != null) {
+            namespaceParts.Push(namespaceDeclaration.Name.ToString());
+            namespaceDeclaration = namespaceDeclaration.Parent as BaseNamespaceDeclarationSyntax;
+        }
+
+        return string.Join(".", namespaceParts);
     }
 }
--- Johnny/ReadGenerator.cs
+++ Johnny/ReadGenerator.cs
@@ -6,57 +6,58 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
-namespace Johnny {
-    public struct GeneratorOptions(bool isLittleEndian) {
-        public bool IsLittleEndian { get; } = isLittleEndian;
+namespace Johnny;
+
+public struct GeneratorOptions(bool isLittleEndian) {
+    public bool IsLittleEndian { get; } = isLittleEndian;
+}
+
+[Generator]
+public class ReadGenerator : IIncrementalGenerator {
+    private static readonly Dictionary<string, string> TypeReaders = new() {
+        { "byte", "ReadByte()" },
+        { "sbyte", "ReadSByte()" },
+        { "short", "ReadInt16()" },
+        { "ushort", "ReadUInt16()" },
+        { "int", "ReadInt32()" },
+        { "uint", "ReadUInt32()" },
+        { "long", "ReadInt64()" },
+        { "ulong", "ReadUInt64()" },
+        { "float", "ReadSingle()" },
+        { "double", "ReadDouble()" },
+        { "bool", "ReadBoolean()" },
+        { "char", "ReadChar()" },
+        { "string", "ReadString()" }
+    };
+
+    public void Initialize(IncrementalGeneratorInitializationContext context) {
+        context.RegisterPostInitializationOutput(ctx =>
+            ctx.AddSource("JohnnyAttribute.g.cs", Data.Attribute.Source));
+
+        var structDeclarations = context.SyntaxProvider
+            .CreateSyntaxProvider(SyntaxHelpers.IsStructWithJohnnyAttribute, SyntaxHelpers.GetStructWithJohnnyAttribute)
+            .Where(static m => m != null);
+
+        var compilationAndStructs = context.CompilationProvider.Combine(structDeclarations.Collect());
+
+        context.RegisterSourceOutput(compilationAndStructs, (spc, source) => Execute(source.Left, source.Right!, spc));
     }
 
-    [Generator]
-    public class ReadGenerator : IIncrementalGenerator {
-        private static readonly Dictionary<string, string> TypeReaders = new() {
-            { "byte", "ReadByte()" },
-            { "sbyte", "ReadSByte()" },
-            { "short", "ReadInt16()" },
-            { "ushort", "ReadUInt16()" },
-            { "int", "ReadInt32()" },
-            { "uint", "ReadUInt32()" },
-            { "long", "ReadInt64()" },
-            { "ulong", "ReadUInt64()" },
-            { "float", "ReadSingle()" },
-            { "double", "ReadDouble()" },
-            { "bool", "ReadBoolean()" },
-            { "char", "ReadChar()" },
-            { "string", "ReadString()" }
-        };
-
-        public void Initialize(IncrementalGeneratorInitializationContext context) {
-            context.RegisterPostInitializationOutput(ctx =>
-                ctx.AddSource("JohnnyAttribute.g.cs", Data.Attribute.Source));
-
-            var structDeclarations = context.SyntaxProvider
-                .CreateSyntaxProvider(SyntaxHelpers.IsStructWithJohnnyAttribute, SyntaxHelpers.GetStructWithJohnnyAttribute)
-                .Where(static m => m != null);
-
-            var compilationAndStructs = context.CompilationProvider.Combine(structDeclarations.Collect());
-
-            context.RegisterSourceOutput(compilationAndStructs, (spc, source) => Execute(source.Left, source.Right!, spc));
+    private static void Execute(Compilation comp, ImmutableArray<StructDeclarationSyntax> structs, SourceProductionContext context) {
+        foreach (var structDecl in structs) {
+            GenerateReadMethodForStruct(structDecl, context);
         }
+    }
 
-        private static void Execute(Compilation comp, ImmutableArray<StructDeclarationSyntax> structs, SourceProductionContext context) {
-            foreach (var structDecl in structs) {
-                GenerateReadMethodForStruct(structDecl, context);
-            }
-        }
+    private static void GenerateReadMethodForStruct(StructDeclarationSyntax structDecl, SourceProductionContext context) {
+        var structName = structDecl.Identifier.Text;
+        var namespaceName = CodegenHelpers.GetNamespace(structDecl);
+        var properties = structDecl.Members
+            .OfType<PropertyDeclarationSyntax>()
+            .Select(p => (Type: p.Type.ToString(), Name: p.Identifier.Text))
+            .ToList();
 
-        private static void GenerateReadMethodForStruct(StructDeclarationSyntax structDecl, SourceProductionContext context) {
-            var structName = structDecl.Identifier.Text;
-            var namespaceName = CodegenHelpers.GetNamespace(structDecl);
-            var properties = structDecl.Members
-                .OfType<PropertyDeclarationSyntax>()
-                .Select(p => (Type: p.Type.ToString(), Name: p.Identifier.Text))
-                .ToList();
-
-            var sourceBuilder = new StringBuilder($@"
+        var sourceBuilder = new StringBuilder($@"
 using System.IO;
 
 namespace {namespaceName}
@@ -68,19 +69,18 @@ namespace {namespaceName}
             var result = new {structName}();
 ");
 
-            foreach (var property in properties) {
-                sourceBuilder.AppendLine(TypeReaders.TryGetValue(property.Type, out var readMethod)
-                    ? $"            result.{property.Name} = reader.{readMethod};"
-                    : $"            result.{property.Name} = {property.Type}.ReadStruct(reader);");
-            }
+        foreach (var property in properties) {
+            sourceBuilder.AppendLine(TypeReaders.TryGetValue(property.Type, out var readMethod)
+                ? $"            result.{property.Name} = reader.{readMethod};"
+                : $"            result.{property.Name} = {property.Type}.ReadStruct(reader);");
+        }
 
-            sourceBuilder.AppendLine(@"
+        sourceBuilder.AppendLine(@"
             return result;
         }
     }
 }");
 
-            context.AddSource($"Johnny_{structName}_Reader.g.cs", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
-        }
+        context.AddSource($"Johnny_{structName}_Reader.g.cs", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
     }
 }
