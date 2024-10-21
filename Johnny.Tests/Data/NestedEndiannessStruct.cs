a701c499fa31146e3b15f0959ee400008cd9e772
glomdom <22115@uktc-bg.com>
2024-10-21 13:17:07 +0300

    chore: test source gen of nested structs of varying endianness

---
+++ Johnny.Tests/Data/NestedEndiannessStruct.cs
@@ -0,0 +1,9 @@
+﻿namespace Johnny.Tests.Data;
+
+[Johnny]
+public partial struct NestedEndiannessStruct {
+    public StructEndianness EndianStruct { get; set; }
+    
+    public Vector3 RandomVector { get; set; }
+    public int RandomNumber { get; set; }
+}
